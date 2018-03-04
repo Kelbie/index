e4991ec64334790bc437a1e6cebe25b0cd1a062c
kevin <notkelbie@outlook.com>
2018-03-04 10:48:42 +0000

    Created navigation page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET home page. */
+router.get('/locked', function(req, res, next) {
+  res.render('locked', { title: 'Express' });
+});
+
 module.exports = router;
