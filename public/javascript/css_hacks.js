bd5fad61f65b13913d03610959d1243bb50ef263
kevin <notkelbie@outlook.com>
2018-05-10 22:42:27 +0100

    backup

--- public/javascript/css_hacks.js
+++
@@ -1,6 +0,0 @@
-function scrollPadding () {
-    /* Function : scrollPadding
-     * ------------------------
-     * Adds the padding to the right side of the horizontal scroll for cryptocurrency selection.
-     */
-}
